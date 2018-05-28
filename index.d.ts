2061c379cccfa4c4606b1e6385cd6f679add8e43
mpv1989 <mark@arangodb.com>
2018-05-28 14:39:41 +0200

    Prepare release

--- index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "mkdirp-promise" {
-  declare function mkdirp(dir: string, opts?: any): Promise<void>;
-  export = mkdirp;
-}
-
-declare module "rmfr" {
-  declare function rmfr(...args: string[]): Promise<void>;
-  export = rmfr;
-}
